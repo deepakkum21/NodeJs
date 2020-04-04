95c0b353a6088d31ad2236918e2ec2b80f679fb1
deepakkum21 <43340283+deepakkum21@users.noreply.github.com>
2020-04-05 00:34:58 +0530

    updating mysql config

--- 8. Using SQL as DB/01-connecting-our-app/util/database.js
+++ 8. Using SQL as DB/01-connecting-our-app/util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
+    database: 'node-schema',
+    password: 'root'
 });
 
 module.exports = pool.promise();
